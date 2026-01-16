e60e57fdc44cb66ebbfa0c9a370295f9b1224223
Shrutipol06 <shrutipol330@gmail.com>
2026-01-16 15:07:51 +0530

    Added Experiment 1 Java programs

---
+++ Experiment-1/Prime.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Prime {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        boolean isPrime = true;
+
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+
+        if(n <= 1) {
+            isPrime = false;
+        } else {
+            for(int i = 2; i < n; i++) {
+                if(n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if(isPrime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime Number");
+
+        sc.close();
+    }
+}
